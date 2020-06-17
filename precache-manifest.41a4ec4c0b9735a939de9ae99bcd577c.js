e0b94eedfc4760565162308a7353b529be2e33da
Eric Bauch <eric.a.bauch@gmail.com>
2020-06-17 09:52:32 -0500

    Updates

---
+++ precache-manifest.41a4ec4c0b9735a939de9ae99bcd577c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ed0170754182309b1dcfa1ebcee709",
+    "url": "/landingPage/index.html"
+  },
+  {
+    "revision": "521c4bb0c2718d7c48bd",
+    "url": "/landingPage/static/css/main.357dc9b1.chunk.css"
+  },
+  {
+    "revision": "3625dc1cada922efb11f",
+    "url": "/landingPage/static/js/2.9572daf5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/landingPage/static/js/2.9572daf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521c4bb0c2718d7c48bd",
+    "url": "/landingPage/static/js/main.a229785b.chunk.js"
+  },
+  {
+    "revision": "2720de50331e609cb169",
+    "url": "/landingPage/static/js/runtime-main.9c22dc95.js"
+  }
+]);
